72dd2c4bc299b3d9dddbc6e59fc28f2e1ccb3770
Durpylord86 <123599265+Durpylord86@users.noreply.github.com>
2025-02-24 16:19:55 -0600

    Update riscv_single.sv

--- lab2/xor/riscv_single.sv
+++ lab2/xor/riscv_single.sv
@@ -287,7 +287,7 @@ endmodule // top
 module imem (input  logic [31:0] a,
 	     output logic [31:0] rd);
    
-   logic [31:0] 		 RAM[63:0];
+	logic [31:0] 		 RAM[1023:0];
    
    assign rd = RAM[a[31:2]]; // word aligned
    
