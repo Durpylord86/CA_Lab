394d3a31b37e7ac222d90622d6e157ce3b44ccc1
Durpylord86 <123599265+Durpylord86@users.noreply.github.com>
2025-03-03 17:06:08 -0600

    Update riscv_single.sv

--- lab2/xor/riscv_single.sv
+++ lab2/xor/riscv_single.sv
@@ -228,8 +228,8 @@ module extend (input  logic [31:7] instr,
        3'b010:  immext = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};       
        // J−type (jal)
        3'b011:  immext = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'b0};
-	// U-type
-	3'b100: immext = {[12{instr[31]}}, instr[31:12]};
+	     // U-type
+	     3'b100: immext = {instr[31:12], 12{instr[1'b0]};
        default: immext = 32'bx; // undefined
      endcase // case (immsrc)
    
