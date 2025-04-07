d38af6c0b59a9f64be4be056955275ef536bb16f
ericasaenz <158093293+ericasaenz@users.noreply.github.com>
2025-04-07 15:15:29 -0500

    Update riscv_pipelined.sv

--- lab3/hdl/riscv_pipelined.sv
+++ lab3/hdl/riscv_pipelined.sv
@@ -275,6 +275,8 @@ module maindec(input  logic [6:0] op,
        7'b0010011: controls = 13'b1_000_0_1_0_00_0_10_0; // I-type ALU
        7'b1101111: controls = 13'b1_011_0_0_0_10_0_00_1; // jal
        7'b0110111: controls = 13'b1_100_1_1_0_00_0_00_0; // lui       
+       //7'b1100111: controls = 14'b1_000_1_0_010_0_xx_1_1; // jalr
+       //7'b0010111: controls = 14'b1_100_x_0_100_0_xx_0_0; // auipc
        7'b0000000: controls = 13'b0_000_0_0_0_00_0_00_0; // need valid values at reset
        default:    controls = 13'bx_xx_x_x_x_xx_x_xx_x; // non-implemented instruction
      endcase
